733f30c54f8ca0567b18340d71b128df3dfb54bc
Brad Johnson <bradj0302@gmail.com>
2022-12-15 17:24:20 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Home from './components/Home';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
+
+export default function App() {
+ 
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="./" element={<Home/>}>
+           
+          </Route>
+          <Route exact path="./search" element={<SearchPage/>}>
+         
+          </Route>
+        </Switch>
+      </Router>
+    <Home />
     </div>
   );
 }
 
-export default App;
+//export default App;
